bff5c4c9ff4123462c19b7aa9d29816824955f34
WangMing <286241793@qq.com>
2018-12-07 11:52:09 +0800

    实现摄像头取色功能

---
+++ ar开发/ar开发/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ar开发
+//
+//  Created by Heaton on 2018/10/19.
+//  Copyright © 2018年 WangMingDeveloper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ar开发/ar开发/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ar开发
+//
+//  Created by Heaton on 2018/10/19.
+//  Copyright © 2018年 WangMingDeveloper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
